8d66fcebffb422c67defd32054420581692f4d62
Riley Martine <riley.martine@protonmail.com>
2022-09-23 16:45:33 -0600

    Add csv trimmer

---
+++ scripts/trim_csv.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Cut unneeded info out of geonames csv, to make parsing faster."""
+
+
+import csv
+from pathlib import Path
+
+# http://download.geonames.org/export/dump/
+# The main 'geoname' table has the following fields :
+# ---------------------------------------------------
+# geonameid         : integer id of record in geonames database
+# name              : name of geographical point (utf8) varchar(200)
+# asciiname         : name of geographical point in plain ascii characters, varchar(200)
+# alternatenames    : alternatenames, comma separated, ascii names automatically transliterated, convenience attribute from alternatename table, varchar(10000)
+# latitude          : latitude in decimal degrees (wgs84)
+# longitude         : longitude in decimal degrees (wgs84)
+# feature class     : see http://www.geonames.org/export/codes.html, char(1)
+# feature code      : see http://www.geonames.org/export/codes.html, varchar(10)
+# country code      : ISO-3166 2-letter country code, 2 characters
+# cc2               : alternate country codes, comma separated, ISO-3166 2-letter country code, 200 characters
+# admin1 code       : fipscode (subject to change to iso code), see exceptions below, see file admin1Codes.txt for display names of this code; varchar(20)
+# admin2 code       : code for the second administrative division, a county in the US, see file admin2Codes.txt; varchar(80)
+# admin3 code       : code for third level administrative division, varchar(20)
+# admin4 code       : code for fourth level administrative division, varchar(20)
+# population        : bigint (8 byte int)
+# elevation         : in meters, integer
+# dem               : digital elevation model, srtm3 or gtopo30, average elevation of 3''x3'' (ca 90mx90m) or 30''x30'' (ca 900mx900m) area in meters, integer. srtm processed by cgiar/ciat.
+# timezone          : the iana timezone id (see file timeZone.txt) varchar(40)
+# modification date : date of last modification in yyyy-MM-dd format
+
+cities = Path("cities15000.csv")
+with cities.open("r") as f:
+    reader = csv.reader(f, dialect=csv.excel_tab)
+    for line in reader:
+        name, lat, long = line[1], float(line[4]), float(line[5])
+        country = line[8]
+        fipscode = line[10]
+        lat = round(lat, 2)
+        long = round(long, 2)
+        population = int(line[14])
+        if population > 50000:  # Only cities with 50k+ population
+            print(f"{name}\t{country}\t{fipscode}\t{lat}\t{long}")
