f0cdfded5c6e279f6aa4c94e078e6532e7b8edf8
Riley Martine <riley.martine@protonmail.com>
2022-09-23 14:02:19 -0600

    Add night percent

--- main.go
+++ main.go
@@ -24,25 +24,48 @@ func main() {
 	// Calculate the sunrise and sunset times
 	sunrise, sunset, err := p.GetSunriseSunset()
 
-	// If no error has occurred, print the results
-	if err == nil {
-		// fmt.Println("Sunrise:", sunrise.Format("15:04:05")) // Sunrise: 06:11:44
-		// fmt.Println("Sunset:", sunset.Format("15:04:05"))   // Sunset: 18:14:27
-		// fmt.Println("Length of apparent solar time in mean solar time:", sunset.Sub(sunrise))
-		//fmt.Println("Solar noon:", sunrise.Add(sunset.Sub(sunrise)/2).Format("15:04:05"))
-
-		if now.Before(sunrise) {
-			fmt.Println("0%")
-		} else if now.After(sunset) {
-			fmt.Println("100%")
-		} else { // After sunrise and before sunset
-			dayDuration := sunset.Sub(sunrise)
-			hasPassed := now.Sub(sunrise)
-			// fmt.Println("Time passed since sunrise:", hasPassed)
-			fractionPassed := hasPassed.Seconds() / dayDuration.Seconds()
-			fmt.Printf("%.0f%%", fractionPassed*100)
-		}
-	} else {
-		fmt.Println(err)
+	if err != nil {
+		panic(err)
 	}
+
+	// fmt.Println("Sunrise:", sunrise.Format("15:04:05")) // Sunrise: 06:11:44
+	// fmt.Println("Sunset:", sunset.Format("15:04:05"))   // Sunset: 18:14:27
+	// fmt.Println("Length of apparent solar time in mean solar time:", sunset.Sub(sunrise))
+	//fmt.Println("Solar noon:", sunrise.Add(sunset.Sub(sunrise)/2).Format("15:04:05"))
+	dayDuration := sunset.Sub(sunrise)
+	if now.After(sunrise) && now.Before(sunset) {
+		hasPassed := now.Sub(sunrise)
+		// fmt.Println("Time passed since sunrise:", hasPassed)
+		fractionPassed := hasPassed.Seconds() / dayDuration.Seconds()
+		fmt.Printf("%.0f%% ☉", fractionPassed*100)
+		return
+	}
+
+	// https://glossary.ametsoc.org/wiki/Mean_solar_day
+	meanDayDuration, err := time.ParseDuration("86400s")
+	if err != nil {
+		panic(err)
+	}
+	nightDuration := meanDayDuration - dayDuration
+
+	if now.Before(sunrise) {
+		nightEnd := sunrise
+		nightStart := nightEnd.Add(-nightDuration)
+		hasPassedNight := now.Sub(nightStart)
+		fractionPassedNight := hasPassedNight.Seconds() / nightDuration.Seconds()
+
+		fmt.Printf("%.0f%% ☾", fractionPassedNight*100)
+		return
+	}
+
+	if now.After(sunset) {
+		nightStart := sunset
+		hasPassedNight := now.Sub(nightStart)
+		fractionPassedNight := hasPassedNight.Seconds() / nightDuration.Seconds()
+
+		fmt.Printf("%.0f%% ☾", fractionPassedNight*100)
+		return
+	}
+
+	panic("This should never happen")
 }
